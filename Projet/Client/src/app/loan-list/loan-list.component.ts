30e4e7b175c507dabead85cedc0a420a4b5750ee
maximegenet1 <maxime.genet.net@gmail.com>
2020-12-17 19:40:31 +0100

    Display BookName + userName for each loan

--- Projet/Client/src/app/loan-list/loan-list.component.ts
+++ Projet/Client/src/app/loan-list/loan-list.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Loan } from '../model/loan';
+import { map, tap } from 'rxjs/operators';
 import { LoanService } from '../services/loan.service';
-
+import { BookService } from '../services/book.service';
+import { UserService } from '../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-loan-list',
@@ -10,23 +13,39 @@ import { LoanService } from '../services/loan.service';
   styleUrls: ['./loan-list.component.css']
 })
 export class LoanListComponent implements OnInit {
+    public loans$: Observable<Loan[]>;
 
-	public loans$: Observable<Loan[]>;
-
-  constructor(private loanService: LoanService) { }
+    constructor(    private router: Router, private bookService: BookService, private userService: UserService, private loanService: LoanService) { }
 
 
-	ngOnInit(): void {
-		this.init()
+    ngOnInit(): void {
+        this.init();
 
-	}
+    }
 
-	public init() {
-		this.loans$ = this.loanService.getAll();
-	}
+    public init() {
+        this.loans$ = this.loanService.getAll()
+            .pipe(
+                tap(this.addInfos.bind(this))
+            );
+    }
 
-	public return(loanId) {
-		this.loanService.return(loanId).subscribe();
-	}
+    private addInfos(loans: Loan[]) {
+        for (const loan of loans) {
+            this.bookService.get(loan.bookId)
+                .pipe(map(book => loan.bookName = book.name))
+                .subscribe();
+
+            this.userService.get(loan.userId)
+                .pipe(map(user => loan.userName = user.name))
+                .subscribe();
+        }
+    }
+
+
+    public return(loanId) {
+        this.loanService.return(loanId).subscribe();
+        this.router.navigateByUrl('/books');
+    }
 
 }
